c90c9362b6a714767500c3b2b9c308a8d0576f2a
Shivam <shivamguptanitw@gmail.com>
2023-05-13 11:36:00 +0530

    Commit 5: Express Layout for View setup ie. layout.ejs

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const App = express();
 const port = 8011;
+const expressLayouts = require("express-ejs-layouts");
+
 
 App.set('view engine', 'ejs');
+App.use(express.static("./Assets"));
+App.use(expressLayouts);
 
 App.use('/',require('./Routes'))
 App.listen(port, ()=>{
