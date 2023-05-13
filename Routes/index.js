a56527967bfaf212b4089701a1cff2dbaffb06c2
Shivam <shivamguptanitw@gmail.com>
2023-05-13 13:07:41 +0530

    Commit 8: Sign Up page created and ready to create databse

--- Routes/index.js
+++ Routes/index.js
@@ -2,5 +2,7 @@ const router = require('express').Router()
 const homectrl = require('../Controller/home')
 
 router.get('/',homectrl.home)
+router.get('/sign-in',homectrl.signIn)
+router.get('/sign-up',homectrl.signUp)
 
 module.exports = router;
