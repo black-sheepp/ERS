f02252477c793cf02db452f93478e58cc608c92b
Shivam <shivamguptanitw@gmail.com>
2023-08-24 22:34:47 +0530

    Commit 32: Archived Employee List UI fixed

--- App.js
+++ App.js
@@ -1,38 +1,37 @@
-const express = require('express')
+const express = require("express");
 const App = express();
 const port = 8011;
 const expressLayouts = require("express-ejs-layouts");
-const bodyParser = require('body-parser')
-const db = require('./Config/mongoose')
+const bodyParser = require("body-parser");
+const db = require("./Config/mongoose");
 const passport = require("passport");
 const session = require("express-session");
 const passportLocal = require("./Config/passport-local-strategy");
 const mongoStore = require("connect-mongo");
-const flash = require('connect-flash');
-const customMWare = require('./Config/middleware');
-const passportGoogle = require('./Config/passport-google-oauth2-strategy');
+const flash = require("connect-flash");
+const customMWare = require("./Config/middleware");
+const passportGoogle = require("./Config/passport-google-oauth2-strategy");
 
-
-App.set('view engine', 'ejs');
-App.use(express.static("./Assets")); 
-App.use('/Uploads',express.static(__dirname + '/Uploads'))
+App.set("view engine", "ejs");
+App.use(express.static("./Assets"));
+App.use("/Uploads", express.static(__dirname + "/Uploads"));
 App.use(expressLayouts);
-App.use(bodyParser.urlencoded({ extended: false }))
-App.use(require('cookie-parser')()); 
+App.use(bodyParser.urlencoded({ extended: false }));
+App.use(require("cookie-parser")());
 App.use(
-    session({
-         name: "ERS",
-         secret: "secret",
-         resave: false,
-         saveUninitialized: true,
-         cookie: {
-              maxAge: 1000 * 60 * 100 * 10,
-         },
-         store: mongoStore.create({
-              mongoUrl: "mongodb://127.0.0.1:27017/ERS_DEV",
-              autoRemove: "disabled", // Default
-         }),
-    })
+	session({
+		name: "ERS",
+		secret: "secret",
+		resave: false,
+		saveUninitialized: true,
+		cookie: {
+			maxAge: 1000 * 60 * 100 * 10,
+		},
+		store: mongoStore.create({
+			mongoUrl: "mongodb://127.0.0.1:27017/ERS_DEV",
+			autoRemove: "disabled", // Default
+		}),
+	})
 );
 // This is the basic express session({..}) initialization.
 App.use(passport.initialize());
@@ -42,12 +41,7 @@ App.use(passport.setAuthenticatedUser);
 App.use(flash());
 App.use(customMWare.setFlash);
 
-
-
-App.use('/',require('./Routes'))
-App.listen(port, ()=>{
-    console.log("Server is up and running on port:",port)
-})
-
-
-
+App.use("/", require("./Routes"));
+App.listen(port, () => {
+	console.log("Server is up and running on port:", port);
+});
