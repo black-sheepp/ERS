ca69244d72369d6feeec88b4941ad1c7c52a7343
Shivam <shivamguptanitw@gmail.com>
2023-10-27 23:23:21 +0530

    Updated

--- Config/mongoose.js
+++ Config/mongoose.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://fullstackmerndevelop:<password>@cluster0.3wruxeu.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb://127.0.0.1:27017')
   .then(() => console.log('MongoDB Connected!'));
